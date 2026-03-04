a306d81ca69e5173842be947215eb3308fb65d70
BenjaminGru <gruberbenji65@gmail.com>
2026-03-04 10:40:48 +0100

    start

--- blaetter_vs_beiser/src/main/java/at/htl/blaetter_vs_beiser/Start.java
+++ blaetter_vs_beiser/src/main/java/at/htl/blaetter_vs_beiser/Start.java
@@ -1,28 +1,17 @@
-package at.htl.blaetter_vs_beiser;
+package at.htl.blaetter_vs_beiser; // Muss in Zeile 1 stehen
 
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
-import javafx.application.Application;
 
-public class start extends GameApplication {
+// Das Wort "public class Start" ist zwingend erforderlich!
+public class Start extends GameApplication {
+
+    @Override
+    protected void initSettings(GameSettings settings) {
+        settings.setTitle("BvB");
+    }
 
     public static void main(String[] args) {
         launch(args);
     }
-
-
-    @Override
-    protected void initSettings(GameSettings gameSettings) {
-
-        gameSettings.setTitle("bvb");
-    }
-    @Override
-    protected void initInput() {}
-
-    @Override
-    protected void initGame() {}
-
-
-
-
 }
