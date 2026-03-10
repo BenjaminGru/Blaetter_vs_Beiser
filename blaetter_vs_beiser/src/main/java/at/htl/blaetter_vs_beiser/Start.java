24606abfa72f324736c5de8f9d47873546675b29
BenjaminGru <gruberbenji65@gmail.com>
2026-03-10 08:20:00 +0100

    First Zombie

--- blaetter_vs_beiser/src/main/java/at/htl/blaetter_vs_beiser/Start.java
+++ blaetter_vs_beiser/src/main/java/at/htl/blaetter_vs_beiser/Start.java
@@ -3,15 +3,32 @@ package at.htl.blaetter_vs_beiser; // Muss in Zeile 1 stehen
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
 
+import static com.almasb.fxgl.dsl.FXGLForKtKt.*;
+
 // Das Wort "public class Start" ist zwingend erforderlich!
 public class Start extends GameApplication {
 
     @Override
     protected void initSettings(GameSettings settings) {
         settings.setTitle("BvB");
+        settings.setFullScreenAllowed(true);
+        settings.setFullScreenFromStart(true);
     }
 
     public static void main(String[] args) {
         launch(args);
+    }
+    @Override
+    protected void initGame() {
+        getGameWorld().addEntityFactory(new Zombie());
+
+        spawn("enemy", getAppWidth(), 100);
+        spawn("enemy", getAppWidth(), 200);
+        spawn("enemy", getAppWidth(), 0);
+
+
+
+
+
     }
 }
--- blaetter_vs_beiser/src/main/java/at/htl/blaetter_vs_beiser/ZombieComponent.java
+++ blaetter_vs_beiser/src/main/java/at/htl/blaetter_vs_beiser/ZombieComponent.java
@@ -1,4 +1,15 @@
 package at.htl.blaetter_vs_beiser;
 
-public class ZombieComponent {
-}
+import com.almasb.fxgl.dsl.FXGL;
+import com.almasb.fxgl.entity.component.Component;
+
+public class ZombieComponent extends Component {
+
+    private double speed = -50;
+
+    @Override
+    public void onUpdate(double tpf) {
+        // tpf (time per frame) sorgt für gleichmäßige Bewegung
+        entity.translateX(speed * tpf);
+
+    }}
